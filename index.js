067e6c3784ce72be206b8797c4ef682d8612f235
Jose Campos <Jose_cmps14@yahoo.com>
2022-11-03 22:00:17 -0500

    commit tree 163e9a83415cf03eea1a95a1afbd3495ad9d2d68

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// code your solution here\
+function saturdayFun(funEvent = 'roller-skate') {
+    return `This Saturday, I want to ${funEvent}!`
+}
+
+function mondayWork (task = 'go to the office') {
+    return `This Monday, I will ${task}.`
+}
+
+
+function wrapAdjective (special = '*') {
+ return function(title = '||') {
+   return `You are ${special}${title}${special}!`
+ }
+
+}
